052566e3ff536aeca63875200b4f98cbf8930a45
cedricd <cedricdecruw@gmail.com>
2023-05-25 18:15:52 +0200

    Updates

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import './index.css';
-import r2wc from '@r2wc/react-to-web-component';
-import MainContainer from './components/main_container/MainContainer';
-
-const R2WCMainContainer = r2wc(MainContainer,{props: {rocrate: {}}});
-
-customElements.define('rocrate-preview-widget', R2WCMainContainer);
